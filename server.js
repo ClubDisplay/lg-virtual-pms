99d1efb88cb64aaee58b1cd8cb71953017db55a4
ClubDisplay <info@clubdisplay.nl>
2026-06-01 10:44:09 +0200

    Read PORT from env variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/autocheckout', (req, res, next) => {
   const { debug, hour, min } = req.query;
